f285d8731007d46b6b454ff2b42b39eef4cf12d3
almaz <almazgimaev@gmail.com>
2025-04-25 11:25:03 +0200

    new step ClassesListSelector

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 from supervisely.app.widgets import (
     Button,
     Card,
+    ClassesListSelector,
     Container,
     FastTable,
     RadioGroup,
@@ -9,6 +10,9 @@ from supervisely.app.widgets import (
     Text,
 )
 
+from sly_sdk.annotation.obj_class import ObjClass
+from sly_sdk.geometry.bitmap import Bitmap
+from sly_sdk.geometry.rectangle import Rectangle
 from sly_sdk.webpy.app import WebPyApplication
 
 # start
@@ -25,36 +29,33 @@ cv_task_radio = RadioGroup(
     ],
     widget_id="cv_task_radio",
 )
-cv_task_radio.disable()
 cv_task_btn = Button("Select", widget_id="cv_task_button")
-cv_task_btn.disable()
 cv_task_card = Card(
     title="Select CV Task",
     content=Container([cv_task_radio, cv_task_btn], widget_id="cv_task_container"),
     widget_id="success_card",
 )
+cv_task_card.disable()
 
 
 # discuss
 discuss_text = Text(text="Discuss", status="info", widget_id="discuss_text")
 discuss_btn = Button("ok", widget_id="discuss_button")
-discuss_btn.disable()
 discuss_card = Card(
     title="Discuss",
     content=Container([discuss_text, discuss_btn], widget_id="discuss_container"),
     widget_id="discuss_card",
 )
+discuss_card.disable()
 
 # training data
 data = [["apples", "21"], ["bananas", "15"]]
 columns = ["Project name", "Items count"]
-columns_options = [
-    {},
-    {"maxValue": 21, "postfix": "images", "tooltip": "description text"},
-]
+columns_options = [{}, {"maxValue": 21, "postfix": "images", "tooltip": "description text"}]
 dataframe = pd.DataFrame(data=data, columns=columns)
 training_data_table = FastTable(
     data=data,
+    columns=columns,
     columns_options=columns_options,
     widget_id="training_data_table",
 )
@@ -70,11 +71,42 @@ training_data_card = Card(
     ),
     widget_id="training_data_card",
 )
+training_data_card.disable()
+
+# classes list selector
+classes = [
+    ObjClass(name="class1", geometry_type=Bitmap),
+    ObjClass(name="class2", geometry_type=Rectangle),
+]
+classes_list_selector = ClassesListSelector(
+    classes=classes,
+    multiple=True,
+    widget_id="classes_list_selector",
+)
+classes_list_selector_btn = Button(
+    "Select Classes",
+    widget_id="classes_list_selector_button",
+)
+classes_list_selector_card = Card(
+    title="Classes List Selector",
+    content=Container(
+        [classes_list_selector, classes_list_selector_btn],
+        widget_id="classes_list_selector_container",
+    ),
+    widget_id="classes_list_selector_card",
+)
+classes_list_selector_card.disable()
 
 
 stepper = Stepper(
-    widgets=[start_card, cv_task_card, discuss_card, training_data_card],
-    titles=["Start", "CV Task", "Discuss", "Training Data"],
+    widgets=[
+        start_card,
+        cv_task_card,
+        discuss_card,
+        training_data_card,
+        classes_list_selector_card,
+    ],
+    titles=["Start", "CV Task", "Discuss", "Training Data", "Select Classes"],
     widget_id="stepper_widget",
 )
 
@@ -91,43 +123,50 @@ app = WebPyApplication(layout=layout)
 @start_btn.click
 def click_start_button():
     stepper.set_active_step(2)
-    start_btn.disable()
-    cv_task_btn.enable()
-    cv_task_radio.enable()
+    cv_task_card.enable()
+    start_card.disable()
 
 
 @cv_task_btn.click
 def click_cv_task_button():
     stepper.set_active_step(3)
-    cv_task_btn.disable()
-    discuss_btn.enable()
-    cv_task_radio.disable()
+    discuss_card.enable()
+    cv_task_card.disable()
 
 
 @discuss_btn.click
 def click_discuss_button():
     stepper.set_active_step(4)
-    discuss_btn.disable()
-    training_data_reset_btn.hide()
-    training_data_text.hide()
-
-
-@training_data_reset_btn.click
-def click_training_data_reset_button():
-    stepper.set_active_step(4)
-    start_btn.disable()
+    training_data_card.enable()
+    discuss_card.disable()
     training_data_reset_btn.hide()
     training_data_text.hide()
 
 
 @training_data_table.row_click
 def row_click(event: FastTable.ClickedRow):
-    stepper.set_active_step(1)
+    stepper.set_active_step(5)
 
     training_data_reset_btn.show()
-    start_btn.enable()
-
     row = event.row
     row_index = event.row_index
     training_data_text.text = f"Selected row: {row_index}. {row}"
     training_data_text.show()
+    training_data_card.disable()
+    classes_list_selector_card.enable()
+
+
+@training_data_reset_btn.click
+def click_training_data_reset_button():
+    stepper.set_active_step(4)
+    training_data_card.enable()
+    classes_list_selector_card.disable()
+    training_data_reset_btn.hide()
+    training_data_text.hide()
+
+
+@classes_list_selector_btn.click
+def click_classes_list_selector_button():
+    stepper.set_active_step(1)
+    classes_list_selector_card.disable()
+    start_card.enable()
