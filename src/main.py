7d14bcd652169965e4b3fad93f622df2c4102f5b
almaz <almazgimaev@gmail.com>
2025-05-28 17:40:51 +0200

    wip

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import src.sly_functions as f
 import src.sly_globals as g
 from src.ui.projects import set_training_data_table
 from src.ui.sidebar import sidebar
-from supervisely import Application
+from supervisely.webpy.app import WebPyApplication
 from supervisely.app.widgets import Container, Dialog
 
 # from sly_sdk.webpy.app import WebPyApplication
@@ -11,10 +11,10 @@ dialog = Dialog(content=sidebar, title="New Experiment")
 dialog.show()
 
 
-layout = Container(widgets=[dialog], widget_id="main_layout_cont")
+layout = Container(widgets=[dialog])
 
 
-app = Application(layout=layout, static_dir="src")
+app = WebPyApplication(layout=layout)
 
 g.api = f._init_api(app)
 
@@ -22,15 +22,23 @@ g.api = f._init_api(app)
 set_training_data_table(g.api)
 
 # btn = Button("Click me")
-# layout = Container(widgets=[btn])
+# editor = Editor(language_mode="yaml")
+# card = Card(
+#     content=Container(widgets=[editor, btn]),
+#     title="Example Card",
+#     description="This is an example card with an editor and a button.",
+# )
+# layout = Container(widgets=[card])
 # app = WebPyApplication(layout=layout)
+
 
 # @btn.click
 # def on_btn_click():
 #     import time
 
+#     print("Button clicked!")
 #     btn.disable()
 #     time.sleep(2)  # Simulate some processing time
-#     time.sleep(1)  # Simulate some processing time
 #     btn.enable()
+#     time.sleep(1)  # Simulate some processing time
 #     btn.hide()
